894facbe6febe77dd57eeed31ec543fd3e3dbed8
weixiangzhe <1532265603@qq.com>
2024-03-04 21:18:31 +0800

    测试2

---
+++ lkd_service/lkd_order_service/src/main/java/com/demo1/q.java
@@ -0,0 +1,12 @@
+package com.demo1;
+
+/**
+ * @Description
+ * @Author susu
+ * @Date 2024/3/4
+ */
+public class q {
+    public static void main(String[] args) {
+        System.out.println(111111);
+    }
+}
